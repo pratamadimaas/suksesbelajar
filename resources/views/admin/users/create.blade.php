fa4b747eebaf99269798f0d9819e1ec7b9c66bd6
pratamadimaas <dimasmg2017@gmail.com>
2025-09-26 15:16:40 +0800

    Update create.blade.php

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -33,22 +33,6 @@
                     @enderror
                 </div>
                 
-                <div class="mb-3">
-                    <label for="password" class="form-label">Kata Sandi</label>
-                    <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password" required>
-                    @error('password')
-                        <div class="invalid-feedback">
-                            {{ $message }}
-                        </div>
-                    @enderror
-                </div>
-
-                <div class="mb-3">
-                    <label for="password_confirmation" class="form-label">Konfirmasi Kata Sandi</label>
-                    <input type="password" class="form-control" 
-                        id="password_confirmation" name="password_confirmation" required>
-                </div>
-                
                 <div class="d-flex justify-content-between mt-4">
                     <a href="{{ route('admin.users.index') }}" class="btn btn-secondary">
                         <i class="bi bi-arrow-left me-1"></i> Kembali
