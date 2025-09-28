957c83f0734d781db442c7667713d059160033c6
pratamadimaas <dimasmg2017@gmail.com>
2025-09-28 08:08:31 +0800

    Update index.blade.php

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -275,6 +275,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('login.form') }}">Login</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register.form') }}">Register</a>
+                        </li>
                     @endauth
                 </ul>
             </div>
